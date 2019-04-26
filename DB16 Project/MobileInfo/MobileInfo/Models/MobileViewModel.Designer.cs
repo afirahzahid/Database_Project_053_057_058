68e0a830012948841b394fed2b7449fc6e7219a3
AlishbaRauf <alishbarauf98@gmail.com>
2019-04-27 02:43:31 +0500

    filters added

--- DB16 Project/MobileInfo/MobileInfo/Models/MobileViewModel.Designer.cs
+++ DB16 Project/MobileInfo/MobileInfo/Models/MobileViewModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Hp\source\repos\MobileInfo\MobileInfo\Models\MobileViewModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Hp\Desktop\db project\DB16 Project\MobileInfo\MobileInfo\Models\MobileViewModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
